7794246d4951afbae12307c0485659e6e4c874b9
Jerowe Tan <j.tan@toyota-mobilitysolutions.ph>
2024-08-04 15:30:01 +0800

    dfsdfd

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import { Elysia } from "elysia";
+import { Env } from "bun";
+import { Context } from "elysia"
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: Context,
+    
+  ): Promise<Response> {
+
+    return await app.fetch(request)
+  },
+}
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
