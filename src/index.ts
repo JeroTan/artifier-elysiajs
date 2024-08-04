24e87a88054b963a06e21c9fdb92d6a2f5f4633f
Jerowe Tan <j.tan@toyota-mobilitysolutions.ph>
2024-08-04 15:38:01 +0800

    SDfsdfs

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,7 @@ import { Context } from "elysia"
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
-export default {
-  async fetch(
-    request: Request,
-    env: Env,
-    ctx: Context,
-    
-  ): Promise<Response> {
 
-    return await app.fetch(request)
-  },
-}
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
